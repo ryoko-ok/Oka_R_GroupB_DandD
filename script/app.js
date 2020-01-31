8b6ddcc7470dcab960453f325edf5586eaf34f50
Oka <r_oka@ats-hr420-dm03.fc.ca>
2020-01-31 13:31:45 -0500

    switch images is workin

---
+++ script/app.js
@@ -0,0 +1,30 @@
+(() => {
+	// set up the puzzle pieces and boards
+	const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
+				puzzlePieces = document. querySelectorAll('.puzzle-pieces img'),
+				gameBoard = document.querySelector('.puzzle-board');
+
+  const pieceNames = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
+
+	function changeImageSet() {
+		// change all athe image elements on the page -> draggable image sources
+		// change the image elements on the left to match the selected puzzle
+		pieceNames.forEach((piece, index) => puzzlePieces[index].src = `images/${piece + this.dataset.puzzleref}.jpg`)
+
+
+
+		// and set the drop zone background image based on the puzzle the user selects
+		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.puzzleref}.jpg)`;
+		//debugger;
+	}
+
+	//add event handling here -> how is the user going to use our app?
+	//what triggers do we need?
+
+	// on the bottom buttons to change the puzzle image we're working with
+	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+
+	// call the fuction and pass in the first nav button as a reference
+	// research call, apply and bind -> look at MDN
+	changeImageSet.call(puzzleButtons[0]);
+})();
